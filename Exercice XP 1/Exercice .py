da974c2b719a3b5c4461ba0e615671a311422db0
Sakadrie Saw <108576116+Sakadrie@users.noreply.github.com>
2022-12-06 15:49:59 +0000

    exercice xp

---
+++ Exercice XP 1/Exercice .py
@@ -0,0 +1,56 @@
+##Exercice 1
+
+print("Hello word \n" *4)
+
+################
+
+##Exercice 2
+a= int(99^3)*8
+print(a)
+#############
+ 
+ ## Exercice 3
+ 
+ 
+ 
+print(5 < 3)
+ 
+print(3 == 3)
+ 
+print(3 =="3")
+
+
+print("hello" =="hello") 
+ 
+ 
+ ###########
+ 
+ ##Exercice 4##
+ 
+ 
+computer_brand= "lenovo"
+print("I have a " + computer_brand  + "computer")
+
+
+##########
+#Exercice 5
+my_name="Adrie"
+my_old="30"
+my_shoe_size="jazz"
+info= "mon nom est", my_name + "j'ai", + my_old + "et ma music preferer est le " + my_shoe_size 
+print(info)
+
+##### Exercice 6
+a= int(input("entrer un entier"))
+b= int(input("entrer un entier"))
+if (a> b) :
+   print("hello word") 
+
+###Exercie 7
+a= int(input("entrer un nombre a:"))
+
+if (a % 2) == 0:
+   print("le nombre est paire".format(a))
+else:
+   print("le nombre est impaire".format(a))
+ 
---
+++ Exercie XP 2/Exercice.py
@@ -0,0 +1,8 @@
+i= 1
+while i <=10:
+    print(i)
+    i=i+1
+print("fini")    
+
+for i in range(10):
+    print(i)
